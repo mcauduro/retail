fcc8e653441297eb9864bf1684e1187d2da5fd5a
sudoamit <sudoamit@amazon.com>
2019-11-14 23:07:22 -0800

    Add CSV sample and pare down gems

---
+++ retail_pos_simulator.rb
@@ -0,0 +1,67 @@
+require 'yaml'
+require 'json'
+require 'holidays'
+require 'faker'
+
+PRODUCTS = [ "staplers", "post-its", "markers", "dry-eraser", "flowers", "beer" ]
+STORES   = [ "SFO1", "SFO2", "SFO3", "LAX", "NYC", "SEA", "BOS" ]
+
+class PointOfSale
+  # simulates a sale at the register
+  def sale_event(time:)
+    is_weekend = weekend?(time)
+
+    demand  = is_weekend ? rand(100..300) : rand(1..50) # if weekend, simulate high demand
+    product = is_weekend ? PRODUCTS[rand(4..5)] : PRODUCTS[rand(0..3)] # choose product depending on weekday/weekend
+
+    {
+      timestamp: time.strftime("%Y-%m-%d %H:00:00"), # Amazon Forecast needs yyyy-mm-dd HH:mm:ss format
+      item_id:   product,
+      demand:    demand,
+      location:  STORES[rand(0..STORES.count-1)]
+    }
+  end
+
+  def aggregate_hourly_sale_events(time:)
+    products_sold = rand(1..PRODUCTS.count-1)
+
+    sale_events = []
+    products_sold.times do |product|
+      sale_events << sale_event(time: time)
+    end
+
+    sale_events
+  end
+
+  private
+
+    def weekend?(time)
+      time.saturday? || time.sunday?
+    end
+end
+
+begin
+  point_of_sale = PointOfSale.new
+  sale_events   = []
+
+  # Generate sales data for 3 months. We will assume stores in different locations
+  # operate from 9a - 6p. We will generate aggregate sales by the hour.
+  from  = Date.parse("Jul 1, 2019")    # start date
+  to    = Date.parse("Sep 30, 2019")   # and end date
+
+  (from..to).each do |date| # for each day between start date and end date...
+    (9..17).each do |hour|    # for each hour in that day...
+      time = Time.parse("#{date.to_s} #{hour}:00:00")
+
+      hourly_sale_events = point_of_sale.aggregate_hourly_sale_events(time: time)
+      hourly_sale_events.each { |sale_event| sale_events << sale_event.values.join(',') }
+    end
+  end
+
+  # write the list (array) of sale events to a CSV file
+  open('retail_analytics.csv', 'a') do |f|
+    f.puts sale_events.sort_by { |k| k[0..18] }  # we need events as a timeseries, so sort by timestamp
+  end
+
+  puts "Done writing events to retail_analytics.csv"
+end
