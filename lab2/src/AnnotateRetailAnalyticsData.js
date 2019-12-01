53431dfb5bafc68aa0d7968b7de4235b843e7c96
sudoamit <sudoamit@amazon.com>
2019-12-01 01:23:50 -0800

    Add more comments to clarify what's going on

--- lab2/src/AnnotateRetailAnalyticsData.js
+++ lab2/src/AnnotateRetailAnalyticsData.js
@@ -2,17 +2,20 @@ const ABOVE_FREEZING_WEATHER = ['sunny', 'rainy', 'cloudy'];
 const BELOW_FREEZING_WEATHER = ['snowy', 'cloudy', 'sunny'];
 
 exports.handler = async (event, context) => {
-    // Process records and annotate them with current weather and temperature
+    // Process each record and annotate them with current weather and temperature
     let annotatedRecords = event.records.map((record) => {
+        // records are base64 encoded, so decode it
         let payload = JSON.parse(Buffer.from(record.data, 'base64').toString());
 
-        // mock up the weather
+        // mock up the weather and add this data to incoming records
         payload.temperature = getRandom(25, 75);
         payload.weather = (payload.temperature < 32) ? BELOW_FREEZING_WEATHER[getRandom(0, 2)] : ABOVE_FREEZING_WEATHER[getRandom(0, 2)];
 
         let annotatedRecord = `${JSON.stringify(payload)}\n`;
+        // uncomment this if testing using mock Lambda events to see the output
         //console.log(`Annotated record:\n ${annotatedRecord}`);
 
+        // base64 encode and then return the new, annotated record back to Firehose
         return {
             recordId: record.recordId,
             result: 'Ok',
